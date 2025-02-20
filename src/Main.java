afe6dc1db48353cd566a9a1582637a83dee5aa0d
“AJamell” <Justroyaldesigns@gmail.com>
2025-02-20 10:02:25 -0500

    a

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Board board = new Board();
-//        Player player1 = new Player("Player 1", board);
-//        Player player2 = new Player("Player 2", board);
-        Dice dice = new Dice();
-    }
-}
---
+++ src/Model/Main.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+//        Model.Player player1 = new Model.Player("Model.Player 1", board);
+//        Model.Player player2 = new Model.Player("Model.Player 2", board);
+        Dice dice = new Dice();
+    }
+}
