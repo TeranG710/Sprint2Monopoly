fc98ddbe5906a782c2f102d5df77a6bf5b7a28db
TeranGmoravian.edu <TeranG@moravian.edu>
2025-03-20 09:51:55 -0400

    Illegal state exception

---
+++ src/Model/Exceptions/IllegalStateException.java
@@ -0,0 +1,5 @@
+package Model.Exceptions;
+
+public class IllegalStateException extends RuntimeException {
+    public IllegalStateException() {super("You must have at least one computer player.");}
+}
